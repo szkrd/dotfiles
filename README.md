a47abf94be9b88ed80ce3ae031695fb414b83988
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2018-07-18 23:48:36 +0200

    feat: differ scripts

--- README.md
+++ README.md
@@ -1,14 +1,43 @@
 dotfiles
 ========
 
-## MacOS
+## Prerequisites
 
-1. update bash
-2. update git
-3. brew install nano
-4. brew install node
-5. brew install diff-so-fancy
+### MacOS
 
-## Arch
+1. [update bash](https://johndjameson.com/blog/updating-your-shell-with-homebrew/)
+2. `brew install git`
+3. `brew install nano`
+4. `brew install node`
+5. `brew cask install meld`
+6. setup node [global installs](prefix=${HOME}/.npm-packages)
+
+### Linux
 
 TODO
+
+### Win
+
+TODO
+
+----
+
+Then do `npm i` as usual.
+
+## Npm scripts
+
+### Diffing
+
+Please set `DIFF_TOOL` env param to your favorite diff tool.
+By default the script uses `meld`.
+
+Filenames may be shortened so far (`bash_profile` as `ba`, `gitignore` as `gi` etc).
+
+- `npm run diff -- bash_profile`
+- `npm run diff:bp`, `npm run diff:ba`, `npm run diff:gi` etc.
+
+## TODO
+
+1. show info about changed files.
+2. add batch merge.
+3. pull or push depending on file modification date.
---
+++ scripts/diff.js
@@ -0,0 +1,48 @@
+const os = require('os')
+const childProcess = require('child_process')
+const path = require('path')
+const chalk = require('chalk')
+const userHome = require('user-home');
+const commandExistsSync = require('command-exists').sync
+
+const getLogger = (color) => (s, ...args) => console.log(chalk[color](s), ...(args.length ? args : []))
+const log = { error: getLogger('red'), warn: getLogger('yellow'), info: getLogger('green') }
+
+const args = process.argv.slice(2);
+const projectLocation = path.join(path.dirname(process.argv[1]), '/..');
+const platform = os.platform()
+const isMacOS = platform === 'darwin'
+const platformNiceName = { darwin: 'macos', linux: 'linux', win: 'win32' }[platform] || platform;
+const diffTool = process.env.DIFF_TOOL || 'meld'
+
+if (!isMacOS) {
+  log.warn(`Platform "${platform}" has not been tasted. It may or may not work.`)
+}
+
+if (!commandExistsSync(diffTool)) {
+  log.error(`Diff tool "${diffTool}" not found, exiting.`)
+  process.exit(1)
+}
+
+if (!args.length) {
+  log.error('Please add a file name as a param, like "bash_profile" (or "bp")!')
+  process.exit(2)
+}
+
+let fn = args[0].replace(/^[_.]/, '')
+fn = {
+  bp: 'bash_profile',
+  ba: 'bash_aliases',
+  gc: 'gitconfig',
+  gi: 'gitignore'
+}[fn] || fn
+
+const subProcess = childProcess.spawn(diffTool, [
+  path.join(projectLocation, 'src', platformNiceName, `_${fn}`),
+  path.join(userHome, `.${fn}`)
+], {
+  detached: true,
+  stdio: 'ignore'
+});
+
+subProcess.unref();
