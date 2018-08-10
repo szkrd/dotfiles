b9a46ca783c6acca6228278760c860d7bbf5abd4
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2018-08-10 21:28:29 +0200

    windows configs

---
+++ src/win/.config/git/git-prompt.sh
@@ -0,0 +1,31 @@
+function is_git_clean() {
+  git_status="$(git status 2> /dev/null)"
+  if [[ -n "$git_status" && ! ${git_status}} =~ "working directory clean" && ! ${git_status}} =~ "working tree clean" ]]; then
+    printf "%b" "\e[1;31m!\e[0m"
+  fi
+}
+
+PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
+PS1="$PS1"'\n'                 # new line
+PS1="$PS1"'\[\033[32m\]'       # change to green
+PS1="$PS1"'\u '                # user<space>
+PS1="$PS1"'\[\033[35m\]'       # change to purple
+PS1="$PS1"'\w'                 # current working directory
+if test -z "$WINELOADERNOEXEC"
+then
+  GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+  COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+  COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+  COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+  if test -f "$COMPLETION_PATH/git-prompt.sh"
+  then
+    . "$COMPLETION_PATH/git-completion.bash"
+    . "$COMPLETION_PATH/git-prompt.sh"
+    PS1="$PS1"'\[\033[36m\]'  # change color to cyan
+    PS1="$PS1"'`__git_ps1`'   # bash function
+    PS1="$PS1"'`is_git_clean`'   # bash function
+  fi
+fi
+PS1="$PS1"'\[\033[0m\]'        # change color
+PS1="$PS1"'\n'                 # new line
+PS1="$PS1"'$ '                 # prompt: always $
